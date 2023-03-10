4a7107723a3d5b2a675b7922fba9e22f7724ebef
Yoshikazu Noda <noda.yoshikazu@gmail.com>
2023-03-10 13:37:33 +0900

    added /index.js to make it node runnable

---
+++ index.js
@@ -0,0 +1,41 @@
+/*
+ * index.js 
+ *
+ * Usage:
+ *  $ node main-ts.js ../tests/assets/serkb2.txt
+ *
+ *
+ *
+ *
+ */
+let fs = require('fs');
+let process = require('process');
+let now = require('performance-now');
+
+let parser = require('./src/index.js');
+
+
+exports.parse = function(filepath_or_text){
+
+  if (fs.existsSync(filepath_or_text))
+    bittext = fs.readFileSync(filepath_or_text, 'utf8');
+  else
+    bittext = filepath_or_text;
+
+  options = {
+    trace: false,
+    debug: false,
+    need_error_report: false,
+  };
+  let bitmark = new parser.BitmarkParser(bittext, options);
+
+  try {
+    let json = bitmark.parse();
+    if (json)
+      console.log(json);
+  }
+  catch(e) {
+    console.error(e);
+  }
+
+};
